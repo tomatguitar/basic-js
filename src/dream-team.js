14621b1cd8e2045200329cb829a843b545b11e76
Dmytro Zhyltsov <zkarman@mail.ru>
2020-10-26 18:27:04 +0200

    feat: add dream-team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(arr) {
+  if (!Array.isArray(arr)) {
+    return false
+  } else {
+    let name = "";
+    for (let i = 0; i < arr.length; i++) {
+      if (typeof arr[i] === "string") {
+        let firstLetter = arr[i].trim().slice(0, 1);
+        name += firstLetter;
+      } 
+    }
+    result = name.toUpperCase().split("").sort().join("");
+    return result;
+  }
+}
