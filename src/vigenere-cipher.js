7c045444b820f5aff33c5b64569b0f492185d3da
Dmytro Zhyltsov <zkarman@mail.ru>
2020-10-27 22:37:00 +0200

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,61 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+  }
+  encrypt(message, key) {
+    if (message == undefined || key == undefined) {
+      throw new Error("ERROR!!");
+    }
+    let upperCaseMessage = [...message.toUpperCase()];
+    let keyLength = 0;
+    let arrKey = key
+      .toUpperCase()
+      .split("")
+      .map((i) => i.charCodeAt() - "A".charCodeAt());
+    upperCaseMessage = upperCaseMessage.map((i) => {
+      if (!/^[A-Z]/.test(i)) {
+        return i;
+      } else {
+        if (keyLength === arrKey.length) keyLength = 0;
+        i = String.fromCharCode(
+          i.charCodeAt() + arrKey[keyLength] <= "Z".charCodeAt()
+            ? i.charCodeAt() + arrKey[keyLength]
+            : i.charCodeAt() + arrKey[keyLength] - 26
+        );
+        keyLength++;
+        return i;
+      }
+    });
+    return this.isDirect
+      ? upperCaseMessage.join("")
+      : upperCaseMessage.reverse().join("");
+  }
+  decrypt(message, key) {
+    if (message == undefined || key == undefined) throw new Error("ERROR!!");
+    let upperCaseMessage = [...message.toUpperCase()];
+    let keyLength = 0;
+    let arrKey = key
+      .split("")
+      .map((i) => i.toUpperCase().charCodeAt() - "A".charCodeAt());
+    upperCaseMessage = upperCaseMessage.map((i) => {
+      if (!/^[A-Z]/.test(i)) {
+        return i;
+      } else {
+        if (keyLength === arrKey.length) keyLength = 0;
+        i = String.fromCharCode(
+          !(i.charCodeAt() - arrKey[keyLength] < "A".charCodeAt())
+            ? i.charCodeAt() - arrKey[keyLength]
+            : i.charCodeAt() - arrKey[keyLength] + 26
+        );
+        keyLength++;
+        return i;
+      }
+    });
+    return this.isDirect
+      ? upperCaseMessage.join("")
+      : upperCaseMessage.reverse().join("");
   }
 }
 
